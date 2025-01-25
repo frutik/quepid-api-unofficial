68dc6c28489b1b29da7e936775440fd0fe8cdbf9
Andrew Kornilov <akornilov@tradetracker.com>
2025-01-25 15:36:49 +0100

    wip

--- quepid_api/openai_utils/__init__.py
+++ quepid_api/openai_utils/__init__.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 from openai import OpenAI
 
+from .prompts import SEARCH_RESULTS_SCRAPPER
+
 logger = logging.getLogger('')
 
 
@@ -46,7 +48,9 @@ def html_to_search(content, markdownify=False, model='gpt-4-turbo', api_key=None
     message = [
         {
             "role": "system",
-            "content": f"You are a web scraper, tasked with extracting data from an online search engine. Your objective is to extract the search query as well as the list of ALL search results from provided content of {content_type} page containing all this information. It's also possible that provided content contains no search results. In this case return empty list of results."
+            "content": SEARCH_RESULTS_SCRAPPER.format(
+                content_type=content_type
+            )
         },
         {
             "role": "user",
---
+++ quepid_api/openai_utils/prompts.py
@@ -0,0 +1,7 @@
+SEARCH_RESULTS_SCRAPPER = """You are a web scraper, 
+tasked with extracting data from an online search engine. 
+Your objective is to extract the search query as well 
+as the list of ALL search results from provided content 
+of {content_type} page containing all this information. 
+It's also possible that provided content contains no 
+search results. In this case return empty list of results."""
