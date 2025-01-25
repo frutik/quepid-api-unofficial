62de4936adeadbe72bb946fef9c38356fa06d461
Andrew Kornilov <akornilov@tradetracker.com>
2025-01-25 10:49:14 +0100

    set debug mode from env parameters

--- quepid_api/quepid_api/settings.py
+++ quepid_api/quepid_api/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = bool(int(os.getenv('DJANGO_DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 
