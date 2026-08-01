29e186f5a7f8796d67016332d32f1bf4f36d4fd9
Andrew Kornilov <akornilov@tradetracker.com>
2026-08-01 20:29:06 +0200

    wip

--- quepid_api/quepid_api/gunicorn.py
+++ quepid_api/quepid_api/gunicorn.py
@@ -1,6 +1,6 @@
 import os
 
-bind = f"0.0.0.0:{os.getenv('GUNICORN_PORT', '8000')}"
+bind = f"0.0.0.0:{os.getenv('GUNICORN_PORT', '8001')}"
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = int(os.getenv("GUNICORN_WORKERS", 1))
 max_requests = int(os.getenv("GUNICORN_MAX_REQUESTS", "1000"))
